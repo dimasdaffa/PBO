4367c0d1362d7c8dc13b8229e83e964c086e56a1
dimasdaffa <dimas.de2004@gmail.com>
2024-05-01 18:48:14 +0700

    revisi week7

--- week1/Mahasiswa.java
+++ week1/Mahasiswa.java
@@ -7,13 +7,19 @@ import week5.MataKuliah;
 
 public class Mahasiswa {
 
-    private String nim, nama;
+    private String nim, nama, tanggallahir;
     private ArrayList<Double> nilai = new ArrayList<Double>();
     private ArrayList<MataKuliah> mk = new ArrayList<MataKuliah>();
 
+    public Mahasiswa(String nim, String nama, String tanggallahir) {
+        this.nim = nim;
+        this.nama = nama;
+        this.tanggallahir = tanggallahir;
+    }
     public Mahasiswa(String nim, String nama) {
         this.nim = nim;
         this.nama = nama;
+
     }
 
     public void insertNilai(double n1) {
@@ -22,6 +28,18 @@ public class Mahasiswa {
 
     public void insertMatkul(String nama, String klp, int sks) {
         mk.add(new MataKuliah(nama, klp, sks));
+    }
+
+    public String getTanggallahir() {
+        return tanggallahir;
+    }
+
+    public String getNama() {
+        return nim;
+    }
+
+    public String getNim() {
+        return nama;
     }
 
     public void belajar() {
